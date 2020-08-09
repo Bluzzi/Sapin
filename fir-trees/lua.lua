34f2dff1d67807af45a50715a0173cba6c9b7598
Bluzzi <bluzzigame@gmail.com>
2020-08-09 23:52:14 +0200

    Add tabulation in lua code

--- fir-trees/lua.lua
+++ fir-trees/lua.lua
@@ -2,9 +2,9 @@ n = io.read()
 l = 1
 
 for i = n, 0, -1 do
-z = string.rep(".", i)
-print(z .. string.rep("x", l) .. z)
-l = l + 2
+    z = string.rep(".", i)
+    print(z .. string.rep("x", l) .. z)
+    l = l + 2
 end
 
 z = string.rep(".", n);
