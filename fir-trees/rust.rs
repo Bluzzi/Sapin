7232e6dd2e64b42f8bb02829ac5d537b505a746e
Seyz <36236750+Seyz123@users.noreply.github.com>
2020-08-10 00:34:54 +0200

    Update rust.rs

--- fir-trees/rust.rs
+++ fir-trees/rust.rs
@@ -2,7 +2,7 @@ use std::io::stdin;
 use std::string::String;
 
 fn main() {
-    let mut str = String::from("");
+    let mut str = String::new();
     stdin().read_line(&mut str);
 
     let n = str.parse::<usize>().unwrap();
