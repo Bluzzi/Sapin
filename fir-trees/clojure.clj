524cd6bffd0376f11b5dee3ffebaf7ffdde39dbf
Bluzzi <bluzzigame@gmail.com>
2020-08-11 01:25:51 +0200

    Update Clojure: remove global variables and use (read) instead of (readline) for console input

--- fir-trees/clojure.clj
+++ fir-trees/clojure.clj
@@ -1,13 +1,9 @@
-(def n (Integer. (read-line)))
-
-(loop [i n l 1]
+(let [n (read)]
+  (loop [i n l 1]
     (when (> i 0)
         (def x (reduce str (repeat i ".")))
         (println (str x (reduce str (repeat l "*")) x))
 
-        (recur (- i 1) (+ l 2))
-    )
-)
-        
-(def z (reduce str (repeat n ".")))
-(println (str z "*" z))
+        (recur (- i 1) (+ l 2))))
+  (let [z (reduce str (repeat n "."))]
+    (println (str z "*" z))))
