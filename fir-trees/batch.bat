31a317c0302b625e5bdd68a3dd2efb46a4cc15f5
Diogolo <diogolololo4269@gmail.com>
2020-08-10 14:22:22 +0200

    fixes

--- fir-trees/batch.bat
+++ fir-trees/batch.bat
@@ -1,6 +1,6 @@
 @echo off
+title sapin
 
-title Sapin
 set /p number=Enter a number :
 
 set l=%number%;
@@ -8,9 +8,9 @@ set length=1
 
 :loop
 
-for /l %%i in (1,1, %l%) do @CALL :REP %%i
-for /l %%i in (1,1, %length%) do @CALl :REP_A %%o
-for /l %%i in (1,1, %l%) do @CALL :REP %%i
+for /l %%i in (1,1, %l%) do CALL :REP %%i
+for /l %%i in (1,1, %length%) do CALl :REP_A %%o
+for /l %%i in (1,1, %l%) do CALL :REP %%i
 echo.
 
 set /a l -= 1
@@ -18,21 +18,16 @@ set /a length +=2
 
 if %l% GTR 0 goto loop
 
-for /l %%i in (1,1, %number%) do @CALL :REP %%i
-for /l %%i in (1,1, 1) do @CALl :REP_A %%o
-for /l %%i in (1,1, %number%) do @CALL :REP %%i
-
+for /l %%i in (1,1, %number%) do CALL :REP %%i
+for /l %%i in (1,1, 1) do CALl :REP_A %%o
+for /l %%i in (1,1, %number%) do CALL :REP %%i
 echo.
-(exit /b)
 
 :REP
-@echo|set /p="."
-@GOTO:EOF
- 
+echo|set /p="."
+GOTO:EOF
 :END
 
 :REP_A
-@echo|set /p="*"
-@GOTO:EOF
- 
-:END
+echo|set /p="*"
+:ENDv
