8429db8f4589c871fc9aea8c4c46f981faa2f634
Bluzzi <bluzzigame@gmail.com>
2021-01-08 20:26:24 +0100

    Clojure: use let instead of def for local variable

--- fir-trees/clojure.clj
+++ fir-trees/clojure.clj
@@ -1,8 +1,8 @@
 (let [n (read)]
   (loop [i n l 1]
     (when (> i 0)
-        (def x (reduce str (repeat i ".")))
-        (println (str x (reduce str (repeat l "*")) x))
+        (let [x (reduce str (repeat i "."))]
+        (println (str x (reduce str (repeat l "*")) x)))
 
         (recur (- i 1) (+ l 2))))
   (let [z (reduce str (repeat n "."))]
